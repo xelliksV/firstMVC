5e84920f81a4d2a33901f74d41c7e07f7a36cdab
Vladislav Pashkov <xelliks666@gmail.com>
2025-01-02 03:01:44 +0700

    Added package model with interface Model abd two classes FakeModel and ModelData

---
+++ task3608/model/FakeModel.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task36.task3608.model;
+
+
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model{
+    private ModelData modelData = new ModelData();
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("A", 1, 1));
+        userList.add(new User("B", 2, 1));
+        modelData.setUsers(userList);
+    }
+}
